fad5d5844947efdd54b5f7992ed44994ec9eab3e
shubhamg2-hexaware <shubhamg2-hexaware>
2018-04-04 16:34:53 +0530

    event handler added

--- widget.js
+++ widget.js
@@ -4,6 +4,13 @@ zChat.init({
     account_key: '5aFslibEbbykmM6aR3wlREgnjA5ubBQe'
 });
 
+zChat.on('connection_update', function(status) {
+    if (status === 'connected') {
+        // Start consuming your API here
+        console.log("connected");
+    }
+})
+
 module.exports = {
 
     "sendMessage": function() {
