e0e093db4022c67350af2ac21d06a0290e2df3b0
shubhamg2-hexaware <shubhamg2-hexaware>
2018-04-05 19:24:43 +0530

    log added

--- index.js
+++ index.js
@@ -34,7 +34,8 @@ app.get('/', function(req, res) {
 
 app.post('/', function(req,res){
   console.log(req.body);
-  console.log(req);
+  console.log(req.req);
+
   res.send("hello");
 })
 // Start the server
